e7df6336ae6c3a4739aacc6cbf1082ae9fb99338
Ansh garg <gargansh2712@gmail.com>
2025-02-08 19:44:01 +0530

    first commit

---
+++ lec3.cpp
@@ -0,0 +1,47 @@
+// for printing 
+//****
+//****
+//****
+//****
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int i = 1;
+//     while(i<=n){
+//         int j =1;
+//         while(j<=n){
+//             cout<<"*";
+//             j=j+1;
+//         }
+//         i=i+1;
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// for printing 
+//111
+//222
+//333
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    int i = 1;
+    while(i<=n){
+        int j = 1;
+        while(j<=n){
+            cout<<i;
+            j=j+1;
+        }
+        i=i+1;
+        cout<<endl;
+    }
+} 
