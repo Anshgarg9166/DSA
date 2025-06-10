050778b6396a16bf1bf234c618302ed2a7f63934
Ansh garg <gargansh2712@gmail.com>
2025-06-10 23:39:06 +0530

    lec 9 ongoing

--- lec008.cpp
+++ lec008.cpp
@@ -109,37 +109,199 @@
 //     }
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int amount;
+//   cout<<"Enter the amount"<<endl;
+//   cin>>amount;
+//   int Rs100 , Rs50 , Rs20 , Rs1;
+
+//   switch(1)
+//   {
+//       case 1: Rs100=amount/100;
+//               amount=amount%100;
+//               cout<<"No of 100 Rupee ="<<Rs100<<endl;
+              
+         
+//       case 2: Rs50=amount/50;
+//               amount=amount%50;
+//               cout<<"No of 50 Rupee ="<<Rs50<<endl;
+              
+
+//       case 3: Rs20=amount/20;
+//               amount=amount%20;
+//               cout<<"No of 20 Rupee ="<<Rs20<<endl;
+               
+
+//       case 4: Rs1=amount/1;
+//               amount=amount%1;
+//               cout<<"No of 1 Rupee ="<<Rs1<<endl; 
+          
+//            }
+//         return 0;
+// }
+
+//Function
+// #include<iostream>
+// using namespace std;
+
+// //fuction declaration
+// int power(int a , int b){
+//         int ans = 1;
+//         for(int i =1;i<=b;i++){
+//                 ans = ans*a;
+//         }
+//         return ans;
+// }
+
+// int main(){
+//         int a,b;
+//         cin>>a>>b;
+
+//         int answer = power(a,b);
+//         cout<<"Answer is "<<answer<<endl;
+// }
+
+// some power function from other method
+// #include<iostream>
+// using namespace std;
+
+// //fuction declaration
+// int power(){
+//         int a , b;
+//         cin>>a>>b;
+//         int ans = 1;
+//         for(int i =1;i<=b;i++){
+//                 ans = ans*a;
+//         }
+//         return ans;
+// }
+
+// int main(){
+//         int answer = power();
+//         cout<<"Answer is "<<answer<<endl;
+// }
+
+
+// even fuction
+// #include<iostream>
+// using namespace std;
+// bool isEven(int a ){
+//         //odd
+//         if(a&1){
+//                 //if and operation of 1 with any number exist means number is odd
+//                 return 0;
+//         }
+
+//         return 1;
+// }
+// int main(){
+//         int num;
+//         cin>>num;
+
+//         if(isEven(num)){
+//                 cout<<"Number is Even";
+//         }
+//         else{
+//                 cout<<"Number is odd";
+//         }
+// }     
+
+//nCr function
+// #include<iostream>
+// using namespace std;
+
+// int factorial(int n){
+//         int fact = 1;
+//         for(int i = 1; i<=n; i++){
+//                 fact = fact*i;
+//         }
+//         return fact;
+// }
+
+// int nCr(int n,int r){
+//         int num = factorial(n);
+//         int deno = factorial(r)*factorial(n-r); 
+//         return num/deno;
+// }
+
+// int main (){
+//         int n,r;
+//         cin >> n >>r;
+//         cout<<"Answer is "<<nCr(n,r)<<endl;
+// }
+
+
+//AP 
+// #include<iostream>
+// using namespace std;
+// int Ap(int n){
+//         return ((3*n)+7);
+// }
+
+// int main(){
+//         int num,answer;
+//         cin>>num;
+//         answer = Ap(num);
+//         cout<<answer;
+//         return 0;
+// }
+
+// total number of set bits in a&b
+// a=2 10
+// b=3 11
+//output = 3
+
+// #include<iostream>
+// using namespace std;
+// int setbit(int num){
+//         int count = 0;
+//         if(num!=0){
+//                 if(num&1 == 1){
+//                         count++;
+//                         num >> 1;
+//                 }
+//                 else{
+//                         num >> 1;
+//                 }
+//         }
+//         return count;
+// }
+// int main(){
+//         int n,answer;
+//         cin>>n;
+//         answer = setbit(n);
+//         cout<<answer;
+// }
+
 #include <iostream>
 using namespace std;
 
+int bits(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        if (n & 1)
+        {
+            sum = sum + 1;
+        }else
+        {
+                n = n >> 1;
+        }
+        
+    }
+    return sum;
+}
+
 int main()
 {
-  int amount;
-  cout<<"Enter the amount"<<endl;
-  cin>>amount;
-  int Rs100 , Rs50 , Rs20 , Rs1;
+    int a, b;
+    cout << "Enter a and b" << endl;
+    cin >> a >> b;
 
-  switch(1)
-  {
-      case 1: Rs100=amount/100;
-              amount=amount%100;
-              cout<<"No of 100 Rupee ="<<Rs100<<endl;
-              
-         
-      case 2: Rs50=amount/50;
-              amount=amount%50;
-              cout<<"No of 50 Rupee ="<<Rs50<<endl;
-              
-
-      case 3: Rs20=amount/20;
-              amount=amount%20;
-              cout<<"No of 20 Rupee ="<<Rs20<<endl;
-               
-
-      case 4: Rs1=amount/1;
-              amount=amount%1;
-              cout<<"No of 1 Rupee ="<<Rs1<<endl; 
-          
-           }
-        return 0;
+    cout << "Total Set Bits = " << bits(a) + bits(b) << endl;
 }
---
+++ lec009.cpp
@@ -0,0 +1,90 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     //declare
+//     int number[15];
+
+//     //acessing an array
+//     cout<<"Value at 14 index "<<number[14]<<endl;
+
+//     //give warning when index is out of range
+//     cout<<"Value at 20 index "<<number[20]<<endl;
+    
+//     int second[3] = {5,7,11};
+//     cout<<"Value at 2 index "<<second[2]<<endl;
+
+//     int third[15] = {2,7};
+//     int n = 15;
+//     for(int i = 0;i<n;i++){
+//         cout<<third[i]<<" ";
+//     }
+//     cout<<endl;
+
+//     int sizeofthird = sizeof(third)/sizeof(int);
+//     cout<<"size of thrid is "<<sizeofthird<<endl;
+
+//     char ch[5] = {'a','b','c','s','d'};
+//     cout<<ch[3]<<endl;
+
+//     return 0;
+// }
+
+#include<iostream>
+#include<climits> // Needed for INT_MIN and INT_MAX
+using namespace std;
+
+// int getMax(int num[],int n){
+//     int max = INT_MIN;
+
+//     for(int i = 0;i<n;i++){
+//         if(num[i]>max){
+//             max = num[i];
+//         }
+//     }
+//     return max;
+// }
+
+
+//Another method
+int getMax(int num[],int n){
+    int maxi = INT_MIN;
+
+    for(int i = 0;i<n;i++){
+        
+        maxi = max(maxi,num[i]);
+        
+        // if(num[i]>max){
+        //     max = num[i];
+        // }
+    }
+    return maxi;
+}
+
+
+int getMin(int num[],int n){
+    int Min = INT_MAX;
+
+    for(int i = 0;i<n;i++){
+        if(num[i] < Min){
+            Min = num[i];
+        }
+    }
+    return Min;
+
+}
+
+int main(){
+    int size;
+    cin>>size;
+
+    int num[100];
+
+    for(int i = 0;i<size;i++){
+        cin>>num[i];
+    }
+
+    cout<<"Maximum number is "<<getMax(num,size)<<endl;
+    cout<<"Minimum number is "<<getMin(num,size)<<endl;
+    return 0;
+}
