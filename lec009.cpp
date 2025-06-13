439257fcdc96e87a61e6bc736356106e0449e560
Ansh garg <gargansh2712@gmail.com>
2025-06-14 00:26:50 +0530

    lec 9 complete

--- lec009.cpp
+++ lec009.cpp
@@ -89,3 +89,87 @@
 //     return 0;
 // }
 
+//Sum of elements in array
+//  #include<iostream>
+//  using namespace std;
+
+//  int sumofarray(int arr[],int n){
+//     int sum = 0;
+//     for(int i = 0 ; i<n;i++){
+//         sum = sum + arr[i];
+//     }
+//     return sum;
+//  }
+
+//  int main(){
+//     int arr[5];
+//     int n = 5;
+//     cout<<"Enter the numbers "<<endl;
+//     for(int i = 0 ; i<n ; i++ ){
+//         cin>>arr[i];
+//     }
+//     int sum = sumofarray(arr,n);
+//     cout<<"Sum of number is "<<sum;
+//     return 0;
+//  }
+
+//linear search
+// #include<iostream>
+// using namespace std;
+
+// bool search(int arr[],int n,int key){
+//     for (int i = 0;i<n;i++){
+//         if(arr[i] == key){
+//             //return 1;
+//             return true;
+//         }
+//     }
+//     //retun 0;
+//     return false;
+// }
+
+// int main(){
+//     int arr[10] = {1,3,4,5,6,7,8,9,10,11};
+//     int key;
+//     cout<<"Enter element for searching "<<endl;
+//     cin>>key;
+
+//     bool found = search(arr,10,key);
+//     if(found){
+//         cout<<"Element is present";
+//     }else{
+//         cout<<"Element is absent";
+//     }
+// }
+
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[],int n){
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[],int n){
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[6] = {1,4,0,5,-2,15};
+    int brr[5] = {2,6,3,9,4};
+
+    reverse(arr,6);
+    reverse(brr,5);
+
+    printArray(arr,6);
+    printArray(brr,5);
+}
