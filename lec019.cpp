bade695bd67b2bab36d596eb00173389145bf408
Ansh garg <gargansh2712@gmail.com>
2025-07-01 23:23:35 +0530

    Lec 19 is completed

--- lec019.cpp
+++ lec019.cpp
@@ -338,3 +338,56 @@
 
 
 //Algorithms 
+
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+int main(){
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(6);
+    v.push_back(7);
+
+    // for binary search
+    cout<<"Search 5 -> "<<binary_search(v.begin(),v.end(),5)<<endl;
+    cout<<"Search 6 -> "<<binary_search(v.begin(),v.end(),6)<<endl;
+
+    //for upper and lower bound (it gives number of values smmaller and greater than the value)
+    cout<<"Lower bound -> "<<lower_bound(v.begin(),v.end(),4)-v.begin()<<endl;
+    cout<<"Upper bound -> "<<upper_bound(v.begin(),v.end(),4)-v.begin()<<endl;
+
+
+    int a = 3;
+    int b = 5;
+    //Max and min
+    cout<<"Max -> "<<max(a,b)<<endl;
+    cout<<"Min -> "<<min(a,b)<<endl;
+
+    //Swap
+    swap(a,b);
+    cout<<"a -> "<<a<<endl;
+
+    string abcd ="abcd";
+    // reverse a string
+    reverse(abcd.begin(),abcd.end());
+    cout<<"String -> "<<abcd<<endl;
+
+    //Rotate
+    rotate(v.begin(),v.begin()+1,v.end());
+    cout<<"After rotate "<<endl;
+    for(int i:v){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    //Sort
+    sort(v.begin(),v.end());
+    for(int i:v){
+        cout<<i<<" ";
+    }
+}
